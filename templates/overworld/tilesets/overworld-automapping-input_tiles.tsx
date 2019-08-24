07d07122a21ba7e2d968ab5bb42f71b3e76d3048
Wiz <the_wizard_who_says_ni@hotmail.com>
2019-08-24 18:38:11 +0200

    Initial commit with WIP overworld template and rules.

---
+++ templates/overworld/tilesets/overworld-automapping-input_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="automapping-input_tiles" tilewidth="8" tileheight="8" tilecount="100" columns="10">
+ <image source="overworld-automapping-input_tiles.png" width="80" height="80"/>
+</tileset>
---
+++ templates/overworld/tilesets/overworld-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="overworld-tileset" tilewidth="8" tileheight="8" tilecount="640" columns="16">
+ <image source="overworld-tileset.png" trans="009057" width="128" height="320"/>
+</tileset>
