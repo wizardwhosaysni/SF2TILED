5eeac642dd91a2edde363c953cb0bf2e1cd438b9
Wiz <the_wizard_who_says_ni@hotmail.com>
2019-09-02 23:16:00 +0200

    Polishing with cleaner random mountains, corners and output fixes.

--- templates/overworld/tilesets/overworld-automapping-input_tiles.tsx
+++ templates/overworld/tilesets/overworld-automapping-input_tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="automapping-input_tiles" tilewidth="8" tileheight="8" tilecount="100" columns="10">
- <image source="overworld-automapping-input_tiles.png" width="80" height="80"/>
+<tileset version="1.2" tiledversion="1.2.4" name="automapping-input_tiles" tilewidth="8" tileheight="8" tilecount="144" columns="12">
+ <image source="overworld-automapping-input_tiles.png" width="96" height="96"/>
 </tileset>
